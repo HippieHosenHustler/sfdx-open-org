484f557c5f804f346b1b3f6dd27a385dd13f44ac
EdwinScharfe <scharfe.edwin@gmail.com>
2023-04-03 12:34:32 +0200

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# sfdx-open-org Changelog
+
+## [Initial Version] - 2023-03-14
---
+++ src/open_org.tsx
@@ -0,0 +1,29 @@
+import { Detail, List } from "@raycast/api";
+import { exec } from "child_process";
+import { usePromise } from "@raycast/utils";
+import { getOrgs } from "./sfdx";
+import { useState } from "react";
+
+export default function Main(): JSX.Element {
+
+  const { data, isLoading } = usePromise(getOrgs, []);
+
+  return (
+      <List
+        isLoading={isLoading}
+        navigationTitle="Orgs"
+      >
+        {data?.map((item) => (
+          <List.Item
+            title={item}
+          ></List.Item>
+        ))}
+      </List>
+  );
+}
+
+function executeOrgList() {
+  exec("sfdx force org list", (error, stdout, stderr) => {
+
+  });
+}
